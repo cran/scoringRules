f7b7df5f1ba32247be92e98095afff2d8fee9eb6
Fabian Krueger <Fabian.Krueger83@gmail.com>
2020-10-05 09:10:02 +0000

    version 1.0.1

--- inst/doc/gettingstarted.R
+++ inst/doc/gettingstarted.R
@@ -1,15 +1,15 @@
-## ----echo = FALSE, fig.align='center', fig.width = 5---------------------
+## ----echo = FALSE, fig.align='center', fig.width = 5--------------------------
 
 grid <- seq(from = -5, to = 10, length.out = 1000)
 plot(x = grid, y = dnorm(grid, mean = 2, sd = 2), bty = "n", type = "l", xlab = "Value", ylab = "Density")
 abline(v = 0, lwd = 2, lty = 2)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 library(scoringRules)
 # CRPS of a normal distribution with mean = standard deviation = 2, outcome is zero
 crps(y = 0, family = "normal", mean = 2, sd = 2)
 
-## ----echo = FALSE, fig.align='center', fig.width = 5---------------------
+## ----echo = FALSE, fig.align='center', fig.width = 5--------------------------
 # Load data
 data(gdp_mcmc)
 
@@ -24,7 +24,7 @@ plot(h, xlab = "Value", ylab = "Probability", main = "")
 # Add vertical line at realizing value
 abline(v = gdp_mcmc$actuals[, "X2012Q4"], lwd = 2, lty = 2)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 # Load data
 data(gdp_mcmc)
 # Get forecast distribution for 2012:Q4
